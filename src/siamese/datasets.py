76d66b324c929751dfb17970b77a2072cfab7a2f
Struan Robertson <contact@struan.tech>
2025-10-10 12:55:21 +0100

    Streaming dataloader
    Fix dataset implementation
    
    Update dataset for use with multiple shoeprints

--- src/siamese/datasets.py
+++ src/siamese/datasets.py
@@ -1,13 +1,13 @@
 """Load datasets using torch.utils.data.Dataset."""
 
-import math
 import random
 from collections import defaultdict
+from dataclasses import dataclass
+from enum import IntEnum, auto
 from pathlib import Path
-from typing import Literal
+from typing import Literal, cast
 
 import torch
-import torchvision
 import torchvision.transforms.functional as F
 from PIL import Image
 from torch.utils.data import Dataset
@@ -15,6 +15,14 @@ from torchvision import transforms
 from tqdm import tqdm
 
 _dataset_mode = Literal["train", "test", "val", "aug_val"]
+
+
+def _find_all_images(path: Path):
+    files = list(path.rglob("*.jpg")) + list(path.rglob("*.png"))
+    if len(files) == 0:
+        raise FileNotFoundError
+
+    return cast(list[Path], files)
 
 
 def calculate_stats(loader: torch.utils.data.DataLoader):
@@ -45,10 +53,7 @@ class IndividualDataset(Dataset):
     def __init__(self, path: Path | str, *, mode: _dataset_mode = "train"):
         path = Path(path).expanduser() / mode
 
-        jpg_files = list(path.rglob("*.jpg"))
-        png_files = list(path.rglob("*.png"))
-
-        self.files = jpg_files + png_files
+        self.files = _find_all_images(path)
 
     def __len__(self):
         return len(self.files)
@@ -58,15 +63,6 @@ class IndividualDataset(Dataset):
         image = Image.open(file).convert("RGB")
 
         return F.to_tensor(image), file
-
-
-def export_augmented(dataset: IndividualDataset, gpu_transform, save_dir: Path):
-    """Export augmented images to a directory."""
-    save_dir = Path(save_dir)
-    save_dir.mkdir(exist_ok=True)
-    for image, file in tqdm(dataset):
-        transformed = gpu_transform(image)
-        torchvision.utils.save_image(transformed, save_dir / file.name)
 
 
 def gpu_transform(
@@ -98,37 +94,6 @@ def gpu_transform(
     return transforms.Compose(transform_list)
 
 
-class RandomOffsetTransormation:
-    """Randomly shift the image in any direction and rotate."""
-
-    def __init__(
-        self,
-        offset: tuple[int, int] = (64, 32),
-        max_rotation: int = 10,
-        scale_diff: float = 0.25,
-    ):
-        self.offset = offset
-        self.max_rotation = max_rotation
-        self.scale_diff = scale_diff
-
-    # TODO check this actually works
-    def __call__(self, img):
-        angle_rad = torch.rand(1).item() * 2 * math.pi
-        dy = int(self.offset[0] * math.sin(angle_rad))
-        dx = int(self.offset[1] * math.cos(angle_rad))
-        rotation = torch.empty(1).uniform_(-self.max_rotation, self.max_rotation).item()
-        scale = torch.empty(1).uniform_(1 - self.scale_diff, 1 + self.scale_diff).item()
-
-        return F.affine(
-            img,
-            angle=rotation,
-            translate=[dx, dy],
-            scale=scale,
-            shear=0.0,  # pyright: ignore [reportArgumentType]
-            fill=1.0,  # pyright: ignore [reportArgumentType]
-        )
-
-
 class LabeledCombinedDataset(Dataset):
     """Load shoeprint and shoemark images. Returns (shoeprint, (shoemarks)) tuples."""
 
@@ -148,12 +113,9 @@ class LabeledCombinedDataset(Dataset):
             shoeprint_path = shoeprint_path / mode
             shoemark_path = shoemark_path / mode
 
-        self.shoeprint_files = list(shoeprint_path.rglob("*.jpg")) + list(
-            shoeprint_path.rglob("*.png")
-        )
+        self.shoeprint_files = _find_all_images(shoeprint_path)
 
-        shoemark_files = list(shoemark_path.rglob("*.jpg")) + list(shoemark_path.rglob("*.png"))
-
+        shoemark_files = _find_all_images(shoemark_path)
         shoemark_classes = defaultdict(list)
 
         for f in shoemark_files:
@@ -201,3 +163,126 @@ class LabeledCombinedDataset(Dataset):
         sample = self[self.current_idx]
         self.current_idx += 1
         return sample
+
+
+class ShoemarkImpressionType(IntEnum):
+    """Enum to describe what kind of impression a returned tensor is."""
+
+    SHOEMARK_NO_BACK = auto()  # Real shoemark with no background
+    SHOEMARK_BACK = auto()  # Real shoemark with a background
+    NO_SHOEMARK = auto()
+
+
+@dataclass
+class ShoemarkImpression:
+    """Container to hold a shoemark and its type."""
+
+    path: Path
+    impression_type: ShoemarkImpressionType
+
+
+class StreamingDataset(Dataset):
+    """Load random floor images and shoeprints."""
+
+    def __init__(
+        self,
+        shoeprint_path: Path,
+        real_shoemark_path: Path,
+        floor_path: Path,
+        image_size: tuple[int, int],
+        min_floor_roi_height: int,
+        synthetic_ratio: float,
+    ):
+        shoeprint_path = (
+            Path(shoeprint_path).expanduser() / "train"
+        )  # Streaming is only used for training
+        real_shoemark_path = Path(real_shoemark_path).expanduser()
+        floor_path = Path(floor_path).expanduser()
+
+        self.shoeprint_files = _find_all_images(shoeprint_path)
+        self.floor_files = _find_all_images(floor_path)
+
+        self.shoemark_files = defaultdict(list)
+
+        def allocate_shoemarks(files: list[Path], impression_type: ShoemarkImpressionType):
+            for f in files:
+                class_id = int(f.stem.split("_")[0])
+                self.shoemark_files[class_id].append(ShoemarkImpression(f, impression_type))
+
+        shoemark_no_back_files = _find_all_images(real_shoemark_path / "no_back")
+        allocate_shoemarks(shoemark_no_back_files, ShoemarkImpressionType.SHOEMARK_NO_BACK)
+
+        shoemark_back_files = _find_all_images(real_shoemark_path / "back")
+        allocate_shoemarks(shoemark_back_files, ShoemarkImpressionType.SHOEMARK_BACK)
+
+        self.image_size = image_size
+        self.min_floor_roi_height = min_floor_roi_height
+        self.synthetic_ratio = synthetic_ratio
+
+    def __len__(self):
+        return len(self.shoeprint_files)
+
+    def __getitem__(self, idx):
+        shoeprint_file = self.shoeprint_files[idx]
+        shoeprint_id = int(shoeprint_file.stem.split("_")[0])
+
+        shoeprint_image = Image.open(shoeprint_file).convert("RGB")
+        shoeprint_image = F.to_tensor(shoeprint_image)
+
+        # For shoeprints that have a shoemark, we don't always want to use the real shoemark
+        use_synthetic = random.random() > self.synthetic_ratio
+
+        if not use_synthetic and shoeprint_id in self.shoemark_files:
+            shoemark = random.choice(self.shoemark_files[shoeprint_id])
+            shoemark_image = Image.open(shoemark.path).convert("RGB")
+            shoemark_image = F.to_tensor(shoemark_image)
+            shoemark_image_type = shoemark.impression_type
+        else:
+            shoemark_image = torch.zeros_like(shoeprint_image)
+            shoemark_image_type = ShoemarkImpressionType.NO_SHOEMARK
+
+        if shoemark_image_type != ShoemarkImpressionType.SHOEMARK_BACK:
+            floor_image = random.choice(self.floor_files)
+            floor_image = self._transform_floor_image(floor_image)
+        else:
+            floor_image = torch.zeros_like(shoeprint_image)
+
+        # The shoemark_image_type will be used for masking shoemark_image and floor_image, as
+        # these are not required for all shoeprints
+        return shoeprint_image, floor_image, shoemark_image, torch.tensor(shoemark_image_type)
+
+    def _transform_floor_image(self, floor_image_path: Path):
+        floor_image = Image.open(floor_image_path).convert("RGB")
+        floor_image = F.to_tensor(floor_image)
+
+        # Randomly rotate by 0, 90, 180 or 270 degrees
+        k = random.randint(0, 3)
+        floor_image = torch.rot90(floor_image, k=k, dims=[-2, -1])
+
+        # Get tensor dimensions
+        _, h, w = floor_image.shape
+
+        # Width must be at least 2*height
+        max_roi_height = min(h, w // 2)
+
+        if max_roi_height < self.min_floor_roi_height:
+            raise ValueError
+
+        # Randomly select height
+        roi_height = random.randint(self.min_floor_roi_height, max_roi_height)
+        roi_width = roi_height * 2  # 2:1 aspect ratio
+
+        # Randomly select top-left corner
+        top = random.randint(0, h - roi_height)
+        left = random.randint(0, w - roi_width)
+
+        # Extract ROI
+        roi = floor_image[:, top : top + roi_height, left : left + roi_width]
+
+        # Scale to 512x256
+        roi = roi.unsqueeze(0)
+        roi = torch.nn.functional.interpolate(
+            roi, size=self.image_size, mode="bilinear", align_corners=False
+        )
+
+        return roi.squeeze(0)
